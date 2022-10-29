bcfcd5dcf90dabf7044a6832778d63eb36e2e03f
Iuriius <105295809+Iuriius@users.noreply.github.com>
2022-10-29 16:23:03 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,31 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
-// const instance = basicLightbox.create(
-//     document.querySelector('#template')
-// )
 console.log(basicLightbox);
 
-const gallery = basicLightbox.create(`
-	<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+const galleryList = document.querySelector('.gallery');
+
+const markup = galleryItems.map(img => `<div class="gallery__item">
+    <a class="gallery__link" href="${img.original}" target="_blank" rel="noopener noreferrer">
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
     />
   </a>
-</div>
-`);
-// const instance = basicLightbox.create(`
-// 	<h1>Not closable</h1>
-// 	<p>It's not possible to close this lightbox with a click.</p>
-// `, {
-//     closable: false
-// })
-// const instance = basicLightbox.create(
-//     document.querySelector('#template')
-// );
-gallery.show();
+</div>`).join('');
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+galleryList.addEventListener('click', e => {
+    if (e.target.nodeName !== 'IMG') return;
+    const instance = basicLightbox.create(`<img src=${e.target.dataset.source} alt=${e.target.alt}/>`);
+    instance.show();
+});
