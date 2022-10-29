13f774ac634d10a0454ebd04f33f7c0bef7d49e4
Iuriius <105295809+Iuriius@users.noreply.github.com>
2022-10-29 12:58:22 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+// const instance = basicLightbox.create(
+//     document.querySelector('#template')
+// )
+console.log(basicLightbox);
+
+const gallery = basicLightbox.create(`
+	<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>
+`);
+// const instance = basicLightbox.create(`
+// 	<h1>Not closable</h1>
+// 	<p>It's not possible to close this lightbox with a click.</p>
+// `, {
+//     closable: false
+// })
+// const instance = basicLightbox.create(
+//     document.querySelector('#template')
+// );
+gallery.show();
