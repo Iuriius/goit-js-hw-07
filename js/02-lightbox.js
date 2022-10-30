d79b9d573863fc136812f889467f27371d241d3e
Iuriius <105295809+Iuriius@users.noreply.github.com>
2022-10-30 16:57:47 +0200

    fin

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,10 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryList = document.querySelector('.gallery');
-new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
 const markup = galleryItems.map(img => `<div class="gallery__item">
     <a class="gallery__item" href="${img.original}">
   <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
 </a>
 </div>`).join('');
 galleryList.insertAdjacentHTML('beforeend', markup);
+new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
